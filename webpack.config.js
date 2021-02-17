cd1c0745d100cea7d26a4c73c49dd1b454f68f9a
finnss <finni-94@hotmail.com>
2021-02-17 12:46:32 +0100

    Try stealing package json from other forks

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require("path");
+
+var EXAMPLES_DIR = path.resolve(__dirname, "docs");
+
+module.exports = {
+  entry: EXAMPLES_DIR + "/main.js",
+  output: {
+    filename: "[name].built.js",
+    chunkFilename: "[id].chunk.built.js",
+    path: "docs",
+    publicPath: "",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel",
+      },
+    ],
+  },
+};
